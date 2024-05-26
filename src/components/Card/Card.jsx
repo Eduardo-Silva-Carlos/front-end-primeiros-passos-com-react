300b5a848d9284f83523855745862c6580bae1e6
Carlos Eduardo <121635641+Eduardo-Silva-Carlos@users.noreply.github.com>
2024-05-26 05:31:22 +0000

    Criação do componente "<Card />"

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,8 @@
+export default function Card() {
+  return (
+    <div className="card">
+      <h1>Java</h1>
+      < img src="https://salvatore.academy/devmon/1_java.png" width="100" />
+    </div>
+  )
+}
